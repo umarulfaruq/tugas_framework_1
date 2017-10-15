f7c554dfb62a5586fa373e9c0a42a2d22f7223f7
umarulfaruq <umarulfaruq09@gmail.com>
2017-10-16 05:11:52 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.scss">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+	<title>Tugas 1</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col header"></div>
+		</div>
+		<div class="row">
+			<div class="col tabel1"></div>
+			<div class="col tabel2"></div>
+			<div class="col tabel3"></div>
+			<div class="col tabel4"></div>
+		</div>
+		<div class="row">
+			<div class="col col1"></div>
+			<div class="col col2"></div>
+		</div>
+		<div class="row">
+			<div class="col col3"></div>
+			<div class="col col4"></div>
+		</div>
+		<div class="row">
+			<div class="col col5"></div>
+			<div class="col col6"></div>
+		</div>
+		<div class="row">
+			<div class="col tabel5"></div>
+			<div class="col tabel6"></div>
+			<div class="col tabel7"></div>
+		</div>
+		<div class="row">
+			<div class="col footer"></div>
+		</div>
+		<div class="row">
+			<div class="col copyright"></div>
+		</div>
+	</div>
+</body>
+</html>
